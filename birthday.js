a7f21f90f535cceeb5a903a8e06f20f695355a68
Maxwell L Rahe <mr03989@srcs.loc>
2018-02-13 11:36:13 -0800

    asdfghjkl;

---
+++ birthday.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function daysUntilBDay(birthday) {
+    var bDay = new Date(birthday);
+    var today = new Date();
+    var days = Math.floor((bDay.getTime() - today.getTime())/1000/60/60/24);
+}
