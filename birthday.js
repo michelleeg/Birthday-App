b20681c20cfeaf3bfbab3b2f436a5c957d214a42
Michelle E Gamble <mg79570@srcs.loc>
2018-02-13 11:44:36 -0800

    displayAge function

--- birthday.js
+++ birthday.js
@@ -1,8 +1,19 @@
-function age {
+function correctBirthdate(date) {
+    var birthInput = new Date(mm/dd/yyyy);
+}
+
+var button1 = document.getElementById('calculateAge');
+
+function calculateAge {
     this.birthDate = new Date(birthDate);
     var today = new Date();
     this.age = function() {
         return Math.floor((new Date() - this.birthDate)/1000/60/60/24/365);
     }
-    return age;
 }
+
+function displayAge() {
+    var input = document.getElementById('inputNum').value;
+    var result = "You are" ;
+}
+button3.addEventListener('click', displayAge);
