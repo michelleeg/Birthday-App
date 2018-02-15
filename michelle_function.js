0302d6211158586c478ebda09bb617bed0899c03
Michelle E Gamble <mg79570@srcs.loc>
2018-02-15 12:19:42 -0800

    fixed how text is displayed to document

--- michelle_function.js
+++ michelle_function.js
@@ -1,6 +1,6 @@
 var button1 = document.getElementById('calculateAge');
 
-function calculateAge {
+function calculateAge(inputNum) {
     this.birthDate = new Date(birthDate);
     var today = new Date();
     this.age = function () {
@@ -10,6 +10,7 @@ function calculateAge {
 
 function displayAge() {
     var input = document.getElementById('inputNum').value;
-    var result = "You are" ;
+    var age = calculateAge(input);
+    document.getElementById('age').textContent = "You are" + calculateAge + "years of age.";
 }
 button3.addEventListener('click', displayAge);
