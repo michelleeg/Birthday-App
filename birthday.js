862e1f884eace8574755d8d83ec69eb4693bd6fd
Maxwell L Rahe <mr03989@srcs.loc>
2018-02-28 11:21:33 -0800

    birthdayCounDown funcion

--- birthday.js
+++ birthday.js
@@ -84,9 +84,26 @@ this.getSign = function () {
 signButton.addEventListener('click',getSign);
 
 }
-function daysUntilBDay(birthday) {
-    var bDay = new Date(birthday);
+
+function birthdayCountDown() {
+
     var today = new Date();
-    var days = Math.floor((bDay.getTime() - today.getTime()) / 1000 / 60 / 60 / 24);
-    return days;
+    var bDay = new Date(document.getElementById('inputDate').value);
+    var currentime = today.getTime();
+    var bDaytime = bDay.getTime();
+
+    if ((bDaytime - currentime) < 0) {
+
+        bDay.setFullYear(today.getFullYear());
+
+        if ((bDay.getTime() - today.getTime()) < 0) {
+            bDay.setFullYear(bDay.getFullYear() + 1);
+        }
+
+    }
+
+    var daysuntil = Math.ceil((bDay.getTime() / 86400000) - (today.getTime() / 86400000));
+
+    return daysuntil;
+
 }
