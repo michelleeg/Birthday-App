f928cf97e3439986ad010bb91fdda30953c56816
Katherine R Linnenkohl <kl88081@srcs.loc>
2018-03-06 11:05:40 -0800

    valid date fixed

--- Valid_Date.js
+++ Valid_Date.js
@@ -1,18 +1,14 @@
 var birthdayButton = document.getElementById();
 
-function validDate(){
+function validDate() {
     var date = documement.getElementById('inputDate').value;
     var invalid = "Sorry, but you did not enter a date in the correct format.";
-    var check1 = str.substring(2,3)
-    var check2 = str.substring(5,6)
-    if (check1 =="/" && check2 == "/"){
-        return ('calculateAll.').value
+    var check1 = str.substring(2, 3)
+    var check2 = str.substring(5, 6)
+    if (check1 == "/" && check2 == "/")
 
-    }else {
-        return(invalid);
-    }
-    if (new Date(date).getTime == true || date.lemgth > 10 || date.length <8) {
-        document.getElementById('errorText').textcontent = sorry;
-    }
+        if (isNaN(new Date(date).getTime) == true || date.lemgth > 10 || date.length < 8) {
+            document.getElementById('errorText').textcontent = sorry;
+        }
 }
 button.addEventListener('click', invalid);
