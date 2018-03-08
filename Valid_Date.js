904c0954d1921ef52a3894403979714ecffd2a43
Katherine R Linnenkohl <kl88081@srcs.loc>
2018-03-08 12:05:37 -0800

    fixing invalid date and html

--- Valid_Date.js
+++ Valid_Date.js
@@ -1,14 +1,32 @@
-var birthdayButton = document.getElementById();
-
-function validDate() {
-    var date = documement.getElementById('inputDate').value;
-    var invalid = "Sorry, but you did not enter a date in the correct format.";
-    var check1 = str.substring(2, 3)
-    var check2 = str.substring(5, 6)
-    if (check1 == "/" && check2 == "/")
-
-        if (isNaN(new Date(date).getTime) == true || date.lemgth > 10 || date.length < 8) {
-            document.getElementById('errorText').textcontent = sorry;
+function checkDate(input) {
+    if (input.length != 10) {
+        return false;
+    } else {
+        var check1 = str.substring(2, 3);
+        var check2 = str.substring(5, 6);
+        if (check1 == "/" && check2 == "/") {
+            return false;
         }
+        var date = new Date(input);
+        if (isNaN(date.getTime()) == true) {
+            return false;
+
+        }
+    }
+    return true;
 }
-button.addEventListener('click', invalid);
+
+function displayFinalResult() {
+    var date = documement.getElementById('inputDate').value;
+    var invalid = "Sorry, but you did not enter a date in the correct format. Please try again.";
+    if (checkDate(input) == false) {
+        document.getElementById('result').textContent = invalid;
+    } else {
+        var result1 = getSign(input);
+        var result2 = bDayCountdown(input);
+        var result3 = calculateAge(input);
+        var finalResult = "You are" + result3 "years old, your zodiac sign is" + result1 ", and there are" + result2 "until your next birthday.";
+        document.getElementById('result').textcontent = finalResult;
+    }
+}
+clickbutton.addEventListener('click', displayFinalResult;)
--- birthday.js
+++ birthday.js
@@ -1,3 +1,5 @@
+var birthdayButton = document.getElementById('calculateAll');
+
 var signButton = document.getElementById('calculate');
 
 function getSign() {
@@ -140,3 +142,37 @@ function displayAge() {
     document.getElementById('age').textContent = age;
 }
 button.addEventListener('click', displayAge);
+
+function checkDate(input) {
+    if (input.length != 10) {
+        return false;
+    } else {
+        var check1 = str.substring(2, 3);
+        var check2 = str.substring(5, 6);
+        if (check1 == "/" && check2 == "/") {
+            return false;
+        }
+        var date = new Date(input);
+        if (isNaN(date.getTime()) == true) {
+            return false;
+
+        }
+    }
+    return true;
+}
+
+function displayFinalResult() {
+    var date = documement.getElementById('inputDate').value;
+    var invalid = "Sorry, but you did not enter a date in the correct format. Please try again.";
+    if (checkDate(input) == false) {
+        document.getElementById('result').textContent = invalid;
+    } else {
+        var result1 = getSign(input);
+        var result2 = bDayCountdown(input);
+        var result3 = calculateAge(input);
+        var finalResult = "You are" + result3 "years old, your zodiac sign is" + result1 ", and there are" + result2 "until your next birthday.";
+        document.getElementById('result').textcontent = finalResult;
+    }
+}
+birthdaybutton.addEventListener('click', displayFinalResult;)
+
