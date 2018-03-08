4e6926dca68de43b3907c3c42ca5504acc9af6c5
Michelle E Gamble <mg79570@srcs.loc>
2018-03-08 11:06:59 -0800

    added calculateAge function to the main js

--- birthday.js
+++ birthday.js
@@ -126,3 +126,17 @@ function dayDisplay() {
 countDown.addEventListener('click', dayDisplay);
 
 
+var button = document.getElementById('calculate');
+
+function calculateAge(inputDate) {
+    var birthday = new Date(document.getElementById('inputDate').value);
+    //var birthday = document.getElementById('inputDate').value
+    var today = new Date();
+    return Math.floor((new Date() - birthday) / 1000 / 60 / 60 / 24 / 365);
+}
+
+function displayAge() {
+    var age = "You are " + calculateAge() + " years of age.";
+    document.getElementById('age').textContent = age;
+}
+button.addEventListener('click', displayAge);
