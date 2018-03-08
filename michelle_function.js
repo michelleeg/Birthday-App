b88a7738adaa419cd784903fa830fcfd7585f0ee
Michelle E Gamble <mg79570@srcs.loc>
2018-03-08 11:05:38 -0800

    the calculateAge function actually works now

--- michelle_function.js
+++ michelle_function.js
@@ -1,14 +1,14 @@
-var button3 = document.getElementById('calculate');
+var button = document.getElementById('calculate');
 
 function calculateAge(inputDate) {
-    var birthday = new Date(inputDate);
+    var birthday = new Date(document.getElementById('inputDate').value);
     //var birthday = document.getElementById('inputDate').value
     var today = new Date();
     return Math.floor((new Date() - birthday) / 1000 / 60 / 60 / 24 / 365);
 }
 
 function displayAge() {
-    var age = calculateAge(inputDate);
-    document.getElementById('age').textContent = "You are " + age + " years of age.";
+    var age = "You are " + calculateAge() + " years of age.";
+    document.getElementById('age').textContent = age;
 }
-button3.addEventListener('click', displayAge);
+button.addEventListener('click', displayAge);
